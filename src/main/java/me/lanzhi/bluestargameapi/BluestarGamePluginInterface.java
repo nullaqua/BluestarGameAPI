dedbf332cec79071aa2ffa2a15023f1655858d79
lanzhi6 <lan_zhi@outlook.com>
2022-07-06 20:40:06 +0800

    [3.2.1]更新前置

--- src/main/java/me/lanzhi/bluestargameapi/BluestarGamePluginInterface.java
+++ src/main/java/me/lanzhi/bluestargameapi/BluestarGamePluginInterface.java
@@ -1,6 +1,6 @@
 package me.lanzhi.bluestargameapi;
 
-import me.lanzhi.bluestarapi.Api.config.YamlFile;
+import me.lanzhi.bluestarapi.api.config.YamlFile;
 import me.lanzhi.bluestargameapi.managers.BluestarGameManagerInterface;
 
 import java.io.File;
